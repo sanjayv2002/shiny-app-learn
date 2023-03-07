f9edf5ab4ec9934c68776c2ca727f84e94ad041a
sanjayv2002 <sanjay.murthy.29@gmail.com>
2023-03-07 20:26:56 +0530

    app-done

--- app.R
+++ app.R
@@ -8,43 +8,54 @@
 #
 
 library(shiny)
+library(palmerpenguins)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Penguin size relationships"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            textInput(inputId = "species",
+                      label= "Species",
+                      value = "Gentoo")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("distPlot"),
+           tableOutput("statsTable")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  # Create size plot for penguin species
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+      ggplot(data = penguins[penguins$species == input$species, ], 
+             mapping = aes(x = bill_length_mm, 
+                           y = bill_depth_mm,
+                           color = sex)) +
+        geom_point() +
+        xlab("Bill length (mm)") +
+        ylab("Bill depth (mm)")       
     })
+    
+    # Add results of a linear model to output
+    output$statsTable <- renderTable({
+      # Create the linear model
+      model <- lm(formula = bill_depth_mm ~ bill_length_mm,
+                  data = penguins[penguins$species == input$species, ])
+      # Extract summary statistics
+      model_summary <- summary(model)
+      # Print coefficient table
+      model_summary$coefficients
+    }, rownames = TRUE)
 }
 
 # Run the application 
